bc690edd6b0153acfb32b33731fbd1cddb88a85a
Leah <leahdodson9@gmail.com>
2021-01-07 17:58:54 -0600

    added card deck for portfolio for uniform card sizing

--- src/components/PortfolioContainer/index.js
+++ src/components/PortfolioContainer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, CardGroup, CardDeck } from 'reactstrap';
 
 import Ascension from '../PortfolioCards/Ascension/index';
 import TGR from '../PortfolioCards/TGR/index';
@@ -8,39 +8,15 @@ import Burgerized from '../PortfolioCards/Burgerized/index';
 
 import "./style.css";
 const PortfolioContainer = () => {
-  return (
-    <div id='portfolioContainer'>
-      <Row id='cardContainer'>
-        <Col xs="auto" sm="3">
-          
-          <Ascension />
-        </Col>
-        <Col xs="auto" sm="3">
-          <TGR />
-        </Col>
-        <Col xs="auto" sm="3">
-          <Clicky />
-        </Col>
-        <Col xs="auto" sm="3">
-          <Burgerized />
-        </Col>
-      </Row>
-      {/*
-      <Row id='cardContainer'>
-        <Col xs="4" sm="3">
-          <Burgerized />
-        </Col>
-        <Col xs="4" sm="3">
-          <TGR />
-        </Col>
-        <Col xs="4" sm="3">
-          <Clicky />
-        </Col>
-      </Row>
-      */}
-      
+return (
+  <div id='portfolioContainer'>
+    <CardDeck id='cardContainer'>
+      <Ascension />
+      <Burgerized />
+      <TGR />
+      <Clicky />
+    </CardDeck>
     </div>
-
   );
 };
 export default PortfolioContainer;
